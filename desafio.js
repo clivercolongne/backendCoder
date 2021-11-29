9fd072ed48587f18b155608579d60224ae842684
clivercolongne <clivercolongne@gmail.com>
2021-11-29 20:23:48 -0300

    actualizacion

--- desafio.js
+++ desafio.js
@@ -1,13 +1,17 @@
 const express = require('express')
+const Contenedor = require('./contenedor.js')
 const app = express()
 const PORT = 8080
+usuario1 = new Contenedor(
+   'cliver.JSON'
+)
 
  const connectedServer = app.listen(PORT, () => {
     console.log(`Servidor Http escuchando en el puerto ${connectedServer.address().port}`)
  })
 
  app.get('/productos', (req, res) => {
-   res.send()
+   res.send(usuario1.getAll())
 })
 
  
