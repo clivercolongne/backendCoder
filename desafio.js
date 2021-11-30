04ab83c139fe351b46311bb53b73ee80fe33ff63
clivercolongne <clivercolongne@gmail.com>
2021-11-29 21:02:17 -0300

    actualizacion 2

--- desafio.js
+++ desafio.js
@@ -4,14 +4,24 @@ const app = express()
 const PORT = 8080
 usuario1 = new Contenedor(
    'cliver.JSON'
-)
+);
 
- const connectedServer = app.listen(PORT, () => {
-    console.log(`Servidor Http escuchando en el puerto ${connectedServer.address().port}`)
- })
+const connectedServer = app.listen(PORT, () => {
+   console.log(`Servidor Http escuchando en el puerto ${connectedServer.address().port}`)
+});
 
- app.get('/productos', (req, res) => {
+app.get('/productos', (req, res) => {
    res.send(usuario1.getAll())
-})
+});
 
+(async () => {
+   await usuario1.save([
+      {
+         "title": "Escuadra",
+         "price": 123.45,
+         "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
+         "id": 1
+      }
+   ])
+})()
  
